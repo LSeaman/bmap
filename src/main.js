dcc52a14bdf652d62deaeed9bb0239dbcb539383
Lseaman <ormeyf@gmail.com>
2020-09-08 14:27:49 +0800

    测试结果

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import BaiduMap from 'vue-baidu-map'
+import '@/components/global.less'
+
+Vue.use(BaiduMap, {
+  // ak 是在百度地图开发者平台申请的密钥 详见 http://lbsyun.baidu.com/apiconsole/key */
+  ak: '7sdh82FdOPGvZ1SEwmsdE1lD3jynWQGA'
+})
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="outside-container">
+    <baidu-map
+      class="map"
+      center="长春"
+      :mapStyle="mapStyle"
+      :zoom="1"
+    ></baidu-map>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      mapStyle: {
+        styleJson: [
+          {
+            featureType: 'all',
+            elementType: 'all',
+            stylers: {
+              lightness: 1,
+              saturation: -100
+            }
+          },
+          {
+            featureType: 'land',
+            elementType: 'all',
+            stylers: {
+              color: '#e2e1ddff'
+            }
+          },
+          {
+            featureType: 'water',
+            elementType: 'all',
+            stylers: {
+              color: '#e2e1dd00'
+            }
+          }
+        ]
+      }
+    }
   }
 }
 </script>
+
+<style lang="less" scoped>
+.outside-container {
+  background: url(~@/assets/timg.jpg);
+  width: 100%;
+  height: 100%;
+  .map {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
